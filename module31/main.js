2a4a8cbb6fb9cc9d1fad6bfaf5adb1782764ea02
amar6420 <amarveliu777@gmail.com>
2026-06-11 20:42:42 +0200

    .

---
+++ module31/main.js
@@ -0,0 +1,31 @@
+for (i = 0; i < 18; i++) {
+    console.log(i);
+}
+
+
+for (i = 0; i < 18; i++) {
+    console.log(i)
+}
+
+var names = ['Steve', 'Bill', 'Elon'];
+
+var x;
+
+for (x of names) {
+    console.log(x)
+}
+
+console.log(names)
+
+
+// challenge
+
+
+
+var name = document.getElementById('name')
+
+var names2 = []
+
+names2.unshift()
+
+console.log(names2);
